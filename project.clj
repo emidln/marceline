ccaa15204844b90018aa482bee63e90c4641bd64
Brandon Adams <emidln@gmail.com>
2015-03-31 10:19:35 -0500

    upgrade storm and clojure

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.emidln/marceline "0.2.2-bja"
+(defproject io.github.emidln/marceline "0.2.2-bja2"
   :description "A Clojure DSL for Storm/Trident."
   :url "https://github.com/yieldbot/marceline"
   :license {:name "Eclipse Public License"
@@ -8,5 +8,5 @@
   :java-source-paths ["src/jvm"]
   :javac-options ["-source" "1.6" "-target" "1.6"]
   :profiles {:provided {:dependencies
-                        [[org.apache.storm/storm-core "0.9.3"]
-                         [org.clojure/clojure "1.5.1"]]}})
+                        [[org.apache.storm/storm-core "0.11.0-SNAPSHOT"]
+                         [org.clojure/clojure "1.6.0"]]}})
